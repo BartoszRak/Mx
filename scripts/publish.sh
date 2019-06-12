1d2c45fcdf685fcefee3fd8701a78cc5790b0928
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-06-12 22:29:40 +0200

    feat: Update publish script.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_BRANCH" = "master" ]
+echo "$TRAVIS_PULL_REQUEST"
+if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]
 then
+  echo "$TRAVIS_PULL_REQUEST"
   yarn lib
   npm version patch --no-git
-  # npm publish
+  npm publish
 fi
