ef8eadd428edc164472c654b9f50397767411ef5
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-06-13 23:47:59 +0200

    feat: Update publish script and increase version.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,6 +6,7 @@ then
   yarn lib
   yarn global add makeshift
   makeshift -r https://registry.npmjs.org/ -t $NPM_TOKEN
-  npm version patch --no-git-tag-version
+  # npm version patch --no-git-tag-version
+  # you have to manuall increase package.json version for npm
   npm publish
 fi
